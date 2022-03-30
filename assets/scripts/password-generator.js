5fed91cafb9d33e9a63fe53afe4aa5cb1fc5b6f0
John Netzel <johnrnetzel@gmail.com>
2022-03-29 20:02:45 -0800

    Add password generation based on single character and user input length

--- assets/scripts/password-generator.js
+++ assets/scripts/password-generator.js
@@ -15,10 +15,8 @@ function generatePassword()
     {
         return; // Can't do anything else without at some possible password characters either
     }
-    window.alert("The character atlas is:\n" + passwordCharAtlas);
-
-    window.alert("Random number chosen is: " + Math.random());
-    return "TODO: Generate Password";
+    
+    return buildPassword();
 
     function verifyPasswordLength()
     {
@@ -60,7 +58,7 @@ function generatePassword()
         const lowercaseAtlas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         const uppercaseAtlas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
         const numeralAtlas = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-        const specialCharAtlas = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~" ];
+        const specialCharAtlas = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
         if(window.confirm("Should the password contain lowercase letters?\nOK for yes, Cancel for no"))
         {
@@ -88,5 +86,21 @@ function generatePassword()
             window.alert("At least one set of characters must be chosen.\nTry again.");
         }
         return isValid;
+    }
+
+    function buildPassword()
+    {
+        var password = "";
+
+        for(var i = 0; i < passwordLength; i++)
+        {
+            password = password.concat(getRandomPasswordChar());
+        }
+        return password;
+
+        function getRandomPasswordChar()
+        {
+            return "*"
+        }
     }
 }
