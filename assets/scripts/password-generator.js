b2348c1f3440c71b67b3eacfa55262a0181ed116
John Netzel <johnrnetzel@gmail.com>
2022-03-29 18:56:18 -0800

    Add validator to ensure that password length entered is a number

--- assets/scripts/password-generator.js
+++ assets/scripts/password-generator.js
@@ -3,7 +3,25 @@ function generatePassword()
     var passwordLength;
 
     passwordLength = window.prompt("Enter the length of your password:\nNote: length must be between 8 and 128 characters");
+    passwordLength = Number.parseInt(passwordLength);
+    if(!verifyPasswordLength())
+        return; // Can't do anything else without a password length
     window.alert("Password length entered is: " + passwordLength);
     window.alert("Random number chosen is: " + Math.random());
     return "TODO: Generate Password";
+
+    function verifyPasswordLength()
+    {
+        return isPasswordLengthNumber();
+    }
+
+    function isPasswordLengthNumber()
+    {
+        var isValid = Number.isSafeInteger(passwordLength);
+        if(!isValid)
+        {
+            window.alert("Password length must be a number.\nTry again.");
+        }
+        return isValid;
+    }
 }
