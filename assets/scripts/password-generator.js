de9c0418027929b47bade4b013d298a571718557
John Netzel <johnrnetzel@gmail.com>
2022-03-29 18:34:52 -0800

    Add prompt for user input of password length

--- assets/scripts/password-generator.js
+++ assets/scripts/password-generator.js
@@ -1,5 +1,9 @@
 function generatePassword()
 {
-    alert("Random number chosen is: " + Math.random());
+    var passwordLength;
+
+    passwordLength = window.prompt("Enter the length of your password:\nNote: length must be between 8 and 128 characters");
+    window.alert("Password length entered is: " + passwordLength);
+    window.alert("Random number chosen is: " + Math.random());
     return "TODO: Generate Password";
 }
