aa4885ce8ea031e5a8237b79dac372c17ebdd559
John Netzel <johnrnetzel@gmail.com>
2022-03-29 19:28:15 -0800

    Add confirm dialogs to enter which sets of characters the user wants in the password

--- assets/scripts/password-generator.js
+++ assets/scripts/password-generator.js
@@ -1,6 +1,7 @@
 function generatePassword()
 {
     var passwordLength;
+    var passwordCharAtlas = [];
 
     passwordLength = window.prompt("Enter the length of your password:\nNote: length must be between 8 and 128 characters");
     passwordLength = Number.parseInt(passwordLength);
@@ -8,7 +9,10 @@ function generatePassword()
     {
         return; // Can't do anything else without a password length
     }
-    
+
+    populatePasswordCharAtlas();
+    window.alert("The character atlas is:\n" + passwordCharAtlas);
+
     window.alert("Random number chosen is: " + Math.random());
     return "TODO: Generate Password";
 
@@ -45,5 +49,26 @@ function generatePassword()
             window.alert("Password length must be no more than 128.\nTry again.");
         }
         return isValid;
+    }
+
+    function populatePasswordCharAtlas()
+    {
+        if(window.confirm("Should the password contain lowercase letters?\nOK for yes, Cancel for no"))
+        {
+            passwordCharAtlas = passwordCharAtlas.concat(["a", "b", "c", "d"]);
+        }
+        console.log(passwordCharAtlas);
+        if(window.confirm("Should the password contain uppercase letters?\nOK for yes, Cancel for no"))
+        {
+            passwordCharAtlas = passwordCharAtlas.concat(["A", "B", "C", "D"]);
+        }
+        if(window.confirm("Should the password contain numerals?\nOK for yes, Cancel for no"))
+        {
+            passwordCharAtlas = passwordCharAtlas.concat(["1", "2", "3", "4"]);
+        }
+        if(window.confirm("Should the password contain special characters?\nOK for yes, Cancel for no"))
+        {
+            passwordCharAtlas = passwordCharAtlas.concat(["!", "@", "#", "$"]);
+        }
     }
 }
