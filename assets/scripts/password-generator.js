8ced840ca4eb16f346aa72c2e160cdbbd6dc697a
John Netzel <johnrnetzel@gmail.com>
2022-03-30 17:07:25 -0800

    Simplify generatePassword() main body by pushing work units into subfunctions

--- assets/scripts/password-generator.js
+++ assets/scripts/password-generator.js
@@ -3,8 +3,7 @@ function generatePassword()
     var passwordLength;
     var passwordCharAtlas = [];
 
-    passwordLength = window.prompt("Enter the length of your password:\nNote: length must be between 8 and 128 characters");
-    passwordLength = Number.parseInt(passwordLength);
+    getPasswordLengthFromUser()
     if(! verifyPasswordLength())
     {
         return; // Can't do anything else without a password length
@@ -17,6 +16,12 @@ function generatePassword()
     }
     
     return buildPassword();
+
+    function getPasswordLengthFromUser()
+    {
+        passwordLength = window.prompt("Enter the length of your password:\nNote: length must be between 8 and 128 characters");
+        passwordLength = Number.parseInt(passwordLength);
+    }
 
     function verifyPasswordLength()
     {
@@ -55,6 +60,7 @@ function generatePassword()
 
     function populatePasswordCharAtlas()
     {
+        // These 4 arrays hold all possible password characters and are grouped so users can select some sets and exclude others
         const lowercaseAtlas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         const uppercaseAtlas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
         const numeralAtlas = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -94,7 +100,7 @@ function generatePassword()
 
         for(var i = 0; i < passwordLength; i++)
         {
-            password = password.concat(getRandomPasswordChar());
+            password = password.concat(getRandomPasswordChar()); // Build the password one character at a time
         }
         return password;
 
